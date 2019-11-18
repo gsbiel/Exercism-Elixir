2eecc4badea58e3f8e59716c88dda79a124413f0
Gabriel <gabriel.sgaspar@yahoo.com.br>
2019-11-17 22:31:17 -0300

    10 tests done

--- resistor-color/lib/resistor_color.ex
+++ resistor-color/lib/resistor_color.ex
@@ -1,11 +1,25 @@
 defmodule ResistorColor do
   @moduledoc false
+  @resistor_code %{
+    "black" => 0,
+    "brown" => 1,
+    "red" => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" => 5,
+    "blue" => 6,
+    "violet" => 7,
+    "grey" => 8,
+    "white" => 9
+  }
 
   @spec colors() :: list(String.t())
   def colors do
+    for {key,_value} <- @resistor_code, do: key
   end
 
   @spec code(String.t()) :: integer()
   def code(color) do
+    @resistor_code[color]
   end
 end
