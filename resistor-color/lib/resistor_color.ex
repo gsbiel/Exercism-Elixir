ae85273ae34a8d4f6ee2694fd028fa6e4331ceb5
Gabriel <gabriel.sgaspar@yahoo.com.br>
2019-11-17 23:28:42 -0300

    resistor color has been done

--- resistor-color/lib/resistor_color.ex
+++ resistor-color/lib/resistor_color.ex
@@ -1,25 +1,26 @@
 defmodule ResistorColor do
   @moduledoc false
-  @resistor_code %{
-    "black" => 0,
-    "brown" => 1,
-    "red" => 2,
-    "orange" => 3,
-    "yellow" => 4,
-    "green" => 5,
-    "blue" => 6,
-    "violet" => 7,
-    "grey" => 8,
-    "white" => 9
-  }
+  @resistor_code [
+    {:black,0},
+    {:brown,1},
+    {:red,2},
+    {:orange,3},
+    {:yellow,4},
+    {:green,5},
+    {:blue,6},
+    {:violet,7},
+    {:grey,8},
+    {:white,9}
+  ]
 
   @spec colors() :: list(String.t())
   def colors do
-    for {key,_value} <- @resistor_code, do: key
+    for {key,_value} <- @resistor_code, do: Atom.to_string(key)
   end
 
   @spec code(String.t()) :: integer()
   def code(color) do
-    @resistor_code[color]
+    String.to_atom(color)
+    |> (&(@resistor_code[&1])).()
   end
 end
