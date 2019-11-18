b122d35c375e48431e414607b7a79defc0c76dda
Gabriel <gabriel.sgaspar@yahoo.com.br>
2019-11-17 21:34:06 -0300

    space-age feito

--- space-age/lib/space_age.ex
+++ space-age/lib/space_age.ex
@@ -1,4 +1,14 @@
 defmodule SpaceAge do
+  @earth_period 31557600
+  @factors %{
+    :mercury => 0.2408467,
+    :venus => 0.61519726,
+    :mars => 1.8808158,
+    :jupiter => 11.862615,
+    :saturn => 29.447498,
+    :uranus => 84.016846,
+    :neptune => 164.79132
+  }
   @type planet ::
           :mercury
           | :venus
@@ -15,5 +25,15 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
+    case planet do
+      :mercury -> seconds/(@factors.mercury*@earth_period)
+      :venus -> seconds/(@factors.venus*@earth_period)
+      :earth -> seconds/@earth_period
+      :mars -> seconds/(@factors.mars*@earth_period)
+      :jupiter -> seconds/(@factors.jupiter*@earth_period)
+      :saturn -> seconds/(@factors.saturn*@earth_period)
+      :uranus -> seconds/(@factors.uranus*@earth_period)
+      :neptune -> seconds/(@factors.neptune*@earth_period)
+    end
   end
 end
